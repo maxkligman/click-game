f889f9141d34735e1ba67dd5ee1fd3410f19c945
Max <mkligman1020@gmail.com>
2019-05-11 14:44:35 -0700

    react deploy gp

---
+++ build/precache-manifest.f944e851eb31b913c4bb5f2879c5f0ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7abe5a70ca23974037675ade0702dc19",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "40d09b44d7ae9abd27f9",
+    "url": "/click-game/static/css/main.b2b37e81.chunk.css"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/click-game/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "40d09b44d7ae9abd27f9",
+    "url": "/click-game/static/js/main.80c2353d.chunk.js"
+  },
+  {
+    "revision": "aa765b97c1e7aaa53c3e",
+    "url": "/click-game/static/js/runtime~main.7d043b49.js"
+  }
+]);
